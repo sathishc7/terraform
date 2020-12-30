cd259332a7e7f008bcc3c28c6547b51e3b83b80e
Sathish <rockersathish@hotmail.com>
2020-12-30 10:14:55 +0530

    updated ec2 module

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,8 +1,25 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "web-app" {
   ami           = "ami-0cda377a1b884a1bc"
   instance_type = "t2.micro"
   key_name      = var.keyname
+  subnet_id     = module.vpc.public_subnet
   tags = {
-    Name = "tf-mod"
+    Name = "web-instance"
   }
+}
+
+
+resource "aws_instance" "db" {
+  ami           = "ami-0cda377a1b884a1bc"
+  instance_type = "t2.micro"
+  key_name      = var.keyname
+  subnet_id     = module.vpc.private_subnet
+  tags = {
+    Name = "db-instance"
+  }
+}
+
+
+module "vpc" {
+  source = "../vpc"
 }
