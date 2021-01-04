a1c6ed45ee743c58254915ec3ca4bfb3dfde5397
Sathish <rockersathish@hotmail.com>
2021-01-04 23:48:49 +0530

    updated ec2 variable values

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 module "ec2" {
     source   = "./modules/ec2"
     keyname = "awscert"
+    public_subnet = module.vpc.public_subnet
+    private_subnet = module.vpc.private_subnet
+    vpc= module.vpc.vpc
 }   
 
 module "vpc" {
